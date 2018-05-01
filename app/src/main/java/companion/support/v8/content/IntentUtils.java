b5cf0429bba66e829a773e9dda749079bb978af3
vitor <vribeiro@infraspeak.com>
2018-05-01 15:46:38 +0100

    Image capture intent

--- app/src/main/java/companion/support/v8/content/IntentUtils.java
+++ app/src/main/java/companion/support/v8/content/IntentUtils.java
@@ -3,9 +3,14 @@ package companion.support.v8.content;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.net.Uri;
 import android.provider.CalendarContract;
+import android.provider.MediaStore;
 import android.text.TextUtils;
+
+import java.util.List;
 
 /**
  * Utility class with intent methods.
@@ -69,4 +74,22 @@ public class IntentUtils {
         }
     }
 
+    /**
+     * Get image capture intent.
+     * @param context of the caller.
+     * @param photoURI for the photo file.
+     * @return intent or null if an error occurs.
+     */
+    public static Intent getImageCaptureIntent(Context context, Uri photoURI) throws Throwable {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        List<ResolveInfo> resolvedIntentActivities = context.getPackageManager().queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
+        for (ResolveInfo resolvedIntentInfo : resolvedIntentActivities) {
+            String packageName = resolvedIntentInfo.activityInfo.packageName;
+            context.grantUriPermission(packageName, photoURI, Intent.FLAG_GRANT_WRITE_URI_PERMISSION | Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        }
+
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+        return intent;
+    }
 }
